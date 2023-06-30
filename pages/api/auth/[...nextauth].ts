fcb941be0b04d658bc283febf11399b065ce70d0
williambeckaskenaizer <william.becaskenaizer@gmail.com>
2023-06-29 19:22:53 -0700

    hotfixed mistake! oops

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -31,20 +31,3 @@ export const authOptions: NextAuthOptions = {
 export default NextAuth({
   ...authOptions,
 });
-
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-
-export default NextAuth({
-  providers : [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID || "",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-      })
-  ],
-  callbacks: {
-    session({ session, token, user }) {
-      return session // The return type will match the one returned in `useSession()`
-    },
-  },
-})
