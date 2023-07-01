5855b8088074e28562106b80d5a14f16dc1ad598
williambeckaskenaizer <william.becaskenaizer@gmail.com>
2023-06-30 22:03:55 -0700

    delete endpoint working, added auth to endpoints

--- pages/api/user/delete.ts
+++ pages/api/user/delete.ts
@@ -3,9 +3,8 @@ import prisma from '../../../lib/prismadb';
 import { authOptions } from "../auth/[...nextauth]";
 import { getServerSession } from "next-auth/next"; 
 
-
 export type UserResponseData = {
-    user: {}
+    message: string;
 }
 
 interface ExtendedNextApiRequest extends NextApiRequest {
@@ -19,32 +18,27 @@ interface ExtendedNextApiRequest extends NextApiRequest {
 
 export default async function handler(req: ExtendedNextApiRequest, res: NextApiResponse<UserResponseData | string>) {
 
+    
     const session = await getServerSession(req, res, authOptions);
 
     if(session?.user?.id !== req.body.userId) {
         return res.status(401).json("Unauthorized");
     }
 
-    let user: any | null = null;
-
+    let deleted: any | null = null;
     const { userId } = req.body;
 
     if (userId) {
-        user = await prisma.user.update({
+        deleted = await prisma.user.delete({
             where: {
                 id: userId
             },
-            data: {
-                image: req.body.image,
-                userName: req.body.userName,
-                bio: req.body.bio
-            }
         })
     }
 
     res.status(200).json(
-        user ? {
-            user: user
+        deleted ? {
+            "message": "User deleted"
         } : "Failed to find user ;("
     );
 }
