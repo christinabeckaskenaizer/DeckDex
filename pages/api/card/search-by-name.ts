ec7b3e847d888e1d6407df2d81ed276adef26735
williambeckaskenaizer <william.becaskenaizer@gmail.com>
2023-07-03 14:36:49 -0700

    added type to search

--- pages/api/card/search-by-name.ts
+++ pages/api/card/search-by-name.ts
@@ -8,6 +8,7 @@ interface ExtendedNextApiRequest extends NextApiRequest {
     body: {
         query: string;
         set?: string;
+        types?: string;
         subtype?: string;
     };
 }
@@ -15,14 +16,14 @@ interface ExtendedNextApiRequest extends NextApiRequest {
 export default async function handler(req: ExtendedNextApiRequest, res: NextApiResponse<UserResponseData | string>) {
 
     let user: any | null = null;
-    const { query, set, subtype } = req.body;
-    let url = `https://api.pokemontcg.io/v2/cards?q=name:"${query}"${set ? ' set.id' + set : ''}${subtype ? ' subtypes:' + subtype : ''}&orderBy=-set.releaseDate`;
-    
+    const { query, set, types, subtype } = req.body;
+    let url = `https://api.pokemontcg.io/v2/cards?q=name:"${query}"${set ? ' set.id' + set : ''}${subtype ? ' subtypes:' + subtype : ''}${types ? ' types:' + types : ''}&orderBy=-set.releaseDate`;
+    console.log(url)
     let result = await fetch(url);
     if (result.status === 200) {
         let resJson = await result.json();
         res.status(200).json(resJson.data);
-    } else{
+    } else {
         res.status(404).json("Failed to find card");
     }
 }
